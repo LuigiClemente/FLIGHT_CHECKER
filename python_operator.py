935004e59a4df10700c9525c67e0aeee9b0fa7b7
LuigiClemente <110490237+LuigiClemente@users.noreply.github.com>
2023-06-01 02:03:45 +0200

    Update python_operator.py

--- python_operator.py
+++ python_operator.py
@@ -1,88 +1,57 @@
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.models import XCom
-from airflow.utils.dates import datetime, timedelta
-from jenkins_operator import JenkinsJobTriggerOperator
-from airflow.sensors.base_sensor_operator import BaseSensorOperator
-from airflow.utils.decorators import apply_defaults
-from airflow.providers.jenkins.hooks.jenkins import JenkinsHook
 import logging
 import os
 import requests
 import json
 import time
 import csv
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
-import pytz  # Import pytz for time zone conversion
+import pytz
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.sensors.base_sensor_operator import BaseSensorOperator
+from airflow.providers.jenkins.operators.jenkins_job_trigger import JenkinsJobTriggerOperator
+from airflow.utils.dates import timedelta
+from airflow.utils.decorators import apply_defaults
 
 load_dotenv()
 
-FLIGHTS_API_KEY = os.getenv("FLIGHTS_API_KEY")
-AIRPORTS = os.getenv("AIRPORTS")
-AIRLINES = os.getenv("AIRLINES")
-DELAY_THRESHOLD = int(os.getenv("DELAY_THRESHOLD", "180"))
-TIME_TO_DEPARTURE_THRESHOLD = int(os.getenv("TIME_TO_DEPARTURE_THRESHOLD", "180"))
-CANCELLED_FLIGHT_TIME_WINDOW_START = int(os.getenv("CANCELLED_FLIGHT_TIME_WINDOW_START", "60"))
-CANCELLED_FLIGHT_TIME_WINDOW_END = int(os.getenv("CANCELLED_FLIGHT_TIME_WINDOW_END", "90"))
-API_HOST = os.getenv("API_HOST", "https://airlabs.co/api/v9")
-API_ENDPOINT = os.getenv("API_ENDPOINT", "schedules")
-IGNORED_DESTINATIONS_BCN = os.getenv("IGNORED_DESTINATIONS_BCN", "").split(",")
-IGNORED_DESTINATIONS_AMS = os.getenv("IGNORED_DESTINATIONS_AMS", "").split(",")
-JENKINS_URL = os.getenv("JENKINS_URL")
-JENKINS_USERNAME = os.getenv("JENKINS_USERNAME")
-JENKINS_JOB_NAME = os.getenv("JENKINS_JOB_NAME")
-JENKINS_TOKEN = os.getenv("JENKINS_TOKEN")
-JENKINS_CONNECTION_ID = os.getenv("JENKINS_CONNECTION_ID")
-API_MAX_RETRIES = int(os.getenv("API_MAX_RETRIES", "5"))
-API_RETRY_DELAY_BASE = int(os.getenv("API_RETRY_DELAY_BASE", "2"))
-REQUIRED_ENV_VARIABLES = os.getenv("REQUIRED_ENV_VARIABLES", "").split(",")
-CSV_FILE_NAME = os.getenv("CSV_FILE_NAME", "conditions_flights.csv")
-API_CALL_TIME_ONGOING_DELAYS = int(os.getenv("API_CALL_TIME_ONGOING_DELAYS", "1"))
-API_CALL_TIME_BEFORE_7AM = int(os.getenv("API_CALL_TIME_BEFORE_7AM", "10"))
-API_CALL_TIME_DEFAULT = int(os.getenv("API_CALL_TIME_DEFAULT", "3"))
-DAG_START_DATE = datetime.strptime(os.getenv("DAG_START_DATE", "2023-05-21"), "%Y-%m-%d")
-DAG_RETRIES = int(os.getenv("DAG_RETRIES", "1"))
-DAG_RETRY_DELAY = timedelta(minutes=int(os.getenv("DAG_RETRY_DELAY", "1")))
-JENKINS_PARAMETERS = json.loads(os.getenv("JENKINS_PARAMETERS", '{"key": "value"}'))
-JENKINS_SLEEP_TIME = int(os.getenv("JENKINS_SLEEP_TIME", "30"))
-JENKINS_MAX_TRIES = int(os.getenv("JENKINS_MAX_TRIES", "10"))
-JENKINS_ALLOWED_STATES = os.getenv("JENKINS_ALLOWED_STATES", "SUCCESS").split(",")
+def validate_environment_variables():
+    """
+    Validates the presence and validity of required environment variables.
+    Raises a ValueError if any of the required variables are missing or empty.
+    """
+    required_variables = [
+        "FLIGHTS_API_KEY", "AIRPORTS", "AIRLINES", "DELAY_THRESHOLD",
+        "TIME_TO_DEPARTURE_THRESHOLD", "CANCELLED_FLIGHT_TIME_WINDOW_START",
+        "CANCELLED_FLIGHT_TIME_WINDOW_END"
+    ]
+    for var in required_variables:
+        if not os.getenv(var):
+            raise ValueError(f"Environment variable {var} is missing or empty")
 
 
 class FlightChecker:
     def __init__(self):
         try:
-            self.api_key = FLIGHTS_API_KEY
-            self.airports = AIRPORTS
-            self.airlines = AIRLINES
-            self.delay_threshold = DELAY_THRESHOLD
-            self.time_to_departure_threshold = TIME_TO_DEPARTURE_THRESHOLD
-            self.cancelled_flight_time_window_start = CANCELLED_FLIGHT_TIME_WINDOW_START
-            self.cancelled_flight_time_window_end = CANCELLED_FLIGHT_TIME_WINDOW_END
-            self.api_host = API_HOST
-            self.api_endpoint = API_ENDPOINT
-            self.ignored_destinations_bcn = IGNORED_DESTINATIONS_BCN
-            self.ignored_destinations_ams = IGNORED_DESTINATIONS_AMS
+            self.api_key = os.getenv("FLIGHTS_API_KEY")
+            self.airports = os.getenv("AIRPORTS")
+            self.airlines = os.getenv("AIRLINES")
+            self.delay_threshold = int(os.getenv("DELAY_THRESHOLD", "180"))
+            self.time_to_departure_threshold = int(os.getenv("TIME_TO_DEPARTURE_THRESHOLD", "180"))
+            self.cancelled_flight_time_window_start = int(os.getenv("CANCELLED_FLIGHT_TIME_WINDOW_START", "60"))
+            self.cancelled_flight_time_window_end = int(os.getenv("CANCELLED_FLIGHT_TIME_WINDOW_END", "90"))
+            self.api_host = os.getenv("API_HOST", "https://airlabs.co/api/v9")
+            self.api_endpoint = os.getenv("API_ENDPOINT", "schedules")
+            self.ignored_destinations_bcn = os.getenv("IGNORED_DESTINATIONS_BCN", "").split(",")
+            self.ignored_destinations_ams = os.getenv("IGNORED_DESTINATIONS_AMS", "").split(",")
             self.last_delay_print_time = {}  # Stores the last delay print time for each airport
 
-            self.validate_environment_variables()
+            validate_environment_variables()
         except Exception as e:
             logging.error(f"Error initializing FlightChecker: {str(e)}")
             raise
-
-    def validate_environment_variables(self):
-        """
-        Validates the presence and validity of required environment variables.
-        Raises a ValueError if any of the required variables are missing or empty.
-        """
-        required_variables = [
-            "FLIGHTS_API_KEY", "AIRPORTS", "AIRLINES", "DELAY_THRESHOLD",
-            "TIME_TO_DEPARTURE_THRESHOLD", "CANCELLED_FLIGHT_TIME_WINDOW_START",
-            "CANCELLED_FLIGHT_TIME_WINDOW_END"
-        ]
-        for var in required_variables:
-            if not os.getenv(var):
-                raise ValueError(f"Environment variable {var} is missing or empty")
 
     def load_flight_data(self, **context):
         """
@@ -94,7 +63,8 @@ class FlightChecker:
         try:
             url = f"{self.api_host}/{self.api_endpoint}?dep_iata={self.airports}&api_key={self.api_key}"
             retry_count = 0
-            max_retries = API_MAX_RETRIES
+            max_retries = int(os.getenv("API_MAX_RETRIES", "5"))
+            retry_delay_base = int(os.getenv("API_RETRY_DELAY_BASE", "2"))
             while retry_count < max_retries:
                 try:
                     response = requests.get(url)
@@ -105,8 +75,8 @@ class FlightChecker:
                     break
                 except requests.exceptions.RequestException as e:
                     logging.error(f"Failed to load flight data: {str(e)}")
-                    logging.info(f"Retrying in {API_RETRY_DELAY_BASE ** retry_count} seconds...")
-                    time.sleep(API_RETRY_DELAY_BASE ** retry_count)
+                    logging.info(f"Retrying in {retry_delay_base ** retry_count} seconds...")
+                    time.sleep(retry_delay_base ** retry_count)
                     retry_count += 1
             else:
                 raise RuntimeError("Failed to load flight data after multiple retries")
@@ -200,7 +170,7 @@ class FlightChecker:
                 logging.warning("Flights list is empty")
                 return
 
-            filename = CSV_FILE_NAME
+            filename = os.getenv("CSV_FILE_NAME", "conditions_flights.csv")
             headers = [
                 'Flight Number',
                 'Departure Airport',
@@ -232,29 +202,17 @@ class FlightChecker:
         """
         if ongoing_delays:
             # If there are ongoing delays, check every hour
-            next_call_time = current_time + timedelta(hours=API_CALL_TIME_ONGOING_DELAYS)
+            api_call_time_ongoing_delays = int(os.getenv("API_CALL_TIME_ONGOING_DELAYS", "1"))
+            next_call_time = current_time + timedelta(hours=api_call_time_ongoing_delays)
         elif current_time.hour < 7:
             # If it's before 7 AM, check at 10 AM
             next_call_time = current_time.replace(hour=10, minute=0, second=0)
         else:
             # Otherwise, check every 3 hours
-            next_call_time = current_time + timedelta(hours=API_CALL_TIME_DEFAULT)
+            api_call_time_default = int(os.getenv("API_CALL_TIME_DEFAULT", "3"))
+            next_call_time = current_time + timedelta(hours=api_call_time_default)
 
         return next_call_time
-
-    def notify_plugin(self, status, flight_info, airport=None, flight_iata=None):
-        """
-        Notifies a plugin or external service about a flight status change.
-        Customize this method to implement the desired notification functionality.
-        Args:
-            status (str): The flight status (e.g., "Delayed", "Cancelled")
-            flight_info (list): Information about the flight
-            airport (str): The airport code
-            flight_iata (str): The flight IATA code
-        """
-        # Example implementation: Log the notification details
-        logging.info(
-            f"Notifying plugin: Status='{status}', Flight Info='{flight_info}', Airport='{airport}', Flight IATA='{flight_iata}'")
 
 
 class DelayedApiCallSensor(BaseSensorOperator):
@@ -282,17 +240,17 @@ class DelayedApiCallSensor(BaseSensorOperator):
 
 
 default_args = {
-    'start_date': DAG_START_DATE,
-    'retries': DAG_RETRIES,
-    'retry_delay': DAG_RETRY_DELAY
+    'start_date': datetime.strptime(os.getenv("DAG_START_DATE", "2023-05-21"), "%Y-%m-%d"),
+    'retries': int(os.getenv("DAG_RETRIES", "1")),
+    'retry_delay': timedelta(minutes=int(os.getenv("DAG_RETRY_DELAY", "1")))
 }
 
 with DAG(
-        'flight_checker',
-        default_args=default_args,
-        description='Flight Checker DAG',
-        schedule_interval=timedelta(minutes=1),
-        catchup=False
+    'flight_checker',
+    default_args=default_args,
+    description='Flight Checker DAG',
+    schedule_interval=timedelta(minutes=1),
+    catchup=False
 ) as dag:
     flight_checker = FlightChecker()
 
@@ -323,14 +281,12 @@ with DAG(
     )
 
     jenkins_trigger = JenkinsJobTriggerOperator(
-        task_id='trigger_jenkins_job',
-        job_name=JENKINS_JOB_NAME,
-        parameters=JENKINS_PARAMETERS,
-        jenkins_connection_id=JENKINS_CONNECTION_ID,
-        sleep_time=JENKINS_SLEEP_TIME,
-        max_try_before_job_appears=JENKINS_MAX_TRIES,
-        allowed_jenkins_states=JENKINS_ALLOWED_STATES,
-        dag=dag,
-    )
+    task_id='trigger_jenkins_job',
+    job_name=os.getenv("JENKINS_JOB_NAME"),
+    jenkins_connection_id=os.getenv("JENKINS_CONNECTION_ID"),
+    parameters=json.loads(os.getenv("JENKINS_PARAMETERS", '{"key": "value"}')),
+    sleep_time=int(os.getenv("JENKINS_SLEEP_TIME", "30")),
+    dag=dag,
+)
 
-    delayed_api_call_sensor >> load_flight_data_task >> analyze_delays_task >> create_csv_file_task >> jenkins_trigger
+delayed_api_call_sensor >> load_flight_data_task >> analyze_delays_task >> create_csv_file_task >> jenkins_trigger
